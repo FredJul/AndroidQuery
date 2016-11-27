e72d168a4f0f9b8dc090eace71da628d427f3b76
Frederic Julian <fred.julian@gmail.com>
2016-11-27 04:24:57 +0100

    fix sample

--- sample/src/main/java/net/frju/androidquery/sample/model/User.java
+++ sample/src/main/java/net/frju/androidquery/sample/model/User.java
@@ -7,7 +7,7 @@ import net.frju.androidquery.sample.provider.LocalDatabaseProvider;
 
 @Table(localDatabaseProvider = LocalDatabaseProvider.class, contentDatabaseProvider = ContentDatabaseProvider.class)
 public class User {
-    @Column(primaryKey = true, realName = "_id")
+    @Column(primaryKey = true, realName = "_id", autoIncrement = true)
     public int id;
     @Column
     public String username;
