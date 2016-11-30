6918c888610c9aab381698edb51ddd7ee708713f
Frederic Julian <fred.julian@gmail.com>
2016-11-30 19:56:31 +0100

    Allow to use iterator and to edit the list of a Result

--- android-query/src/main/java/net/frju/androidquery/operation/function/Result.java
+++ android-query/src/main/java/net/frju/androidquery/operation/function/Result.java
@@ -6,10 +6,28 @@ import android.database.CursorWrapper;
 import net.frju.androidquery.database.Resolver;
 import net.frju.androidquery.database.TableDescription;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-public class Result<T> extends CursorWrapper {
+public class Result<T> extends CursorWrapper implements Iterable<T> {
+
+    public class ResultIterator implements Iterator<T> {
+
+        public boolean hasNext() {
+            return getCount() > 0 && getPosition() < getCount() - 1;
+        }
+
+        public T next() {
+            moveToNext();
+            return get();
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("Cannot remove item from Result");
+        }
+    }
 
     private TableDescription mQuery;
 
@@ -32,6 +50,11 @@ public class Result<T> extends CursorWrapper {
     }
 
     public List<T> toList() {
-        return Arrays.asList(toArray());
+        return new ArrayList<>(Arrays.asList(toArray())); // need to copy the array otherwise it's not modifiable
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ResultIterator();
     }
 }
