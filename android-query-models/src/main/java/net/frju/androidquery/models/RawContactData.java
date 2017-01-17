965f900a2fc6cca1a21e996b2c4682a3de4b8dca
Frederic Julian <fred.julian@gmail.com>
2017-01-18 00:05:21 +0100

    v1.5.0

--- android-query-models/src/main/java/net/frju/androidquery/models/RawContactData.java
+++ android-query-models/src/main/java/net/frju/androidquery/models/RawContactData.java
@@ -79,6 +79,48 @@ public class RawContactData {
         }
     }
 
+    public enum PhoneType {
+        HOME(ContactsContract.CommonDataKinds.Phone.TYPE_HOME),
+        MOBILE(ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE),
+        WORK(ContactsContract.CommonDataKinds.Phone.TYPE_WORK),
+        FAX_WORK(ContactsContract.CommonDataKinds.Phone.TYPE_FAX_WORK),
+        FAX_HOME(ContactsContract.CommonDataKinds.Phone.TYPE_FAX_HOME),
+        PAGER(ContactsContract.CommonDataKinds.Phone.TYPE_PAGER),
+        OTHER(ContactsContract.CommonDataKinds.Phone.TYPE_OTHER),
+        CALLBACK(ContactsContract.CommonDataKinds.Phone.TYPE_CALLBACK),
+        CAR(ContactsContract.CommonDataKinds.Phone.TYPE_CAR),
+        COMPANY_MAIN(ContactsContract.CommonDataKinds.Phone.TYPE_COMPANY_MAIN),
+        ISDN(ContactsContract.CommonDataKinds.Phone.TYPE_ISDN),
+        MAIN(ContactsContract.CommonDataKinds.Phone.TYPE_MAIN),
+        OTHER_FAX(ContactsContract.CommonDataKinds.Phone.TYPE_OTHER_FAX),
+        RADIO(ContactsContract.CommonDataKinds.Phone.TYPE_RADIO),
+        TELEX(ContactsContract.CommonDataKinds.Phone.TYPE_TELEX),
+        TTY_TDD(ContactsContract.CommonDataKinds.Phone.TYPE_TTY_TDD),
+        WORK_MOBILE(ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE),
+        WORK_PAGER(ContactsContract.CommonDataKinds.Phone.TYPE_WORK_PAGER),
+        ASSISTANT(ContactsContract.CommonDataKinds.Phone.TYPE_ASSISTANT),
+        MMS(ContactsContract.CommonDataKinds.Phone.TYPE_MMS);
+
+        private final int type;
+
+        PhoneType(final int text) {
+            this.type = text;
+        }
+
+        public int toInt() {
+            return type;
+        }
+
+        public static PhoneType fromInt(int type) {
+            for (PhoneType item : PhoneType.values()) {
+                if (type == item.type) {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+
     /**
      * The unique ID for a row.
      */
@@ -241,9 +283,34 @@ public class RawContactData {
     }
 
     /**
-     * The nickname type. One of the {@link NicknameType}.toString() types.
+     * The nickname type. One of the {@link NicknameType} types.
      */
-    public String getNickname_type() {
-        return data2;
+    public NicknameType getNickname_type() {
+        return NicknameType.fromInt(Integer.valueOf(data2));
+    }
+
+
+    /**
+     * The phone number as the user entered it.
+     */
+    public String getPhone_number() {
+        return data1;
+    }
+
+    /**
+     * The phone number's E164 representation. This value can be omitted in which
+     * case the provider will try to automatically infer it.  (It'll be left null if the
+     * provider fails to infer.)
+     * If present, {@link #getPhone_number()} has to be set as well (it will be ignored otherwise).
+     */
+    public String getPhone_normalizedNumber() {
+        return data4;
+    }
+
+    /**
+     * The phone type. One of the {@link PhoneType} types.
+     */
+    public PhoneType getPhone_type() {
+        return PhoneType.fromInt(Integer.valueOf(data2));
     }
 }
