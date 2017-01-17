5b3961dc4e1687dc46a71df58ff0d8a6a81e2ae1
Frederic Julian <fred.julian@gmail.com>
2017-01-17 13:38:50 +0100

    fix compilation

--- sample/src/main/java/net/frju/androidquery/sample/loader/ContactsLoader.java
+++ sample/src/main/java/net/frju/androidquery/sample/loader/ContactsLoader.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import net.frju.androidquery.database.BaseSelectLoader;
 import net.frju.androidquery.models.Contact;
-import net.frju.androidquery.models.gen.Q;
+import net.frju.androidquery.models.gen.CONTACT;
 import net.frju.androidquery.operation.condition.Where;
 import net.frju.androidquery.operation.function.CursorResult;
 import net.frju.androidquery.operation.keyword.OrderBy;
@@ -17,9 +17,9 @@ public class ContactsLoader extends BaseSelectLoader<Contact> {
 
     @Override
     public CursorResult<Contact> doSelect() {
-        return Q.CONTACT.select()
-                .where(Where.field(Q.CONTACT.IN_VISIBLE_GROUP).isEqualTo(true))
-                .orderBy(new OrderBy(Q.CONTACT.DISPLAY_NAME, OrderBy.Order.ASC, OrderBy.Collate.LOCALIZED))
+        return CONTACT.select()
+                .where(Where.field(CONTACT.IN_VISIBLE_GROUP).isEqualTo(true))
+                .orderBy(new OrderBy(CONTACT.DISPLAY_NAME, OrderBy.Order.ASC, OrderBy.Collate.LOCALIZED))
                 .query();
     }
 }
